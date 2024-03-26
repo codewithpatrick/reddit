77e63edc699df5cbd64742fc9679bad38627b7c6
Patrick Pangilinan <programmingwithpatrick@gmail.com>
2024-03-26 11:59:10 +0800

    Add Sign Up and Enhance Login

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,7 +1,9 @@
 import { authModalState } from '@/src/atoms/authModalAtom'
-import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useRecoilState } from 'recoil'
+import AuthInputs from './AuthInputs'
+import OAuthButtons from './OAuthButtons'
 
 const AuthModal = () => {
     const [modalState, setModalState] = useRecoilState(authModalState);
@@ -17,10 +19,26 @@ const AuthModal = () => {
             <Modal isOpen={modalState.open} onClose={handleClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalHeader textAlign="center">
+                        {modalState.view === "login" && "Login"}
+                        {modalState.view === "signup" && "Sign Up"}
+                        {modalState.view === "resetPassword" && "Reset Password"}
+                    </ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>
-                        This is the modal.
+                    <ModalBody display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        pb={6}>
+                        <Flex direction="column"
+                            align="center"
+                            justify="center"
+                            width="70%">
+                            <OAuthButtons />
+                            <Text color="gray.400"
+                                fontWeight={700}>OR</Text>
+                            <AuthInputs />
+                        </Flex>
                     </ModalBody>
                 </ModalContent>
             </Modal>
---
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -0,0 +1,21 @@
+import { Button, Flex, Image } from '@chakra-ui/react'
+import React from 'react'
+
+const OAuthButtons = () => {
+    return (
+        <Flex direction="column"
+            width="100%"
+            mb={4}>
+            <Button variant="oath"
+                mb={2}>
+                <Image src="/images/googlelogo.png"
+                    height="20px"
+                    mr={4} />
+                Continue with Google
+            </Button>
+            <Button variant="oath">Some Other Provider</Button>
+        </Flex>
+    )
+}
+
+export default OAuthButtons
