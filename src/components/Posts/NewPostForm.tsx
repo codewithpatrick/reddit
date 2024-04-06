efc26018623e00d80eb88fbeb867ecfdaea848ea
Patrick Pangilinan <programmingwithpatrick@gmail.com>
2024-04-06 23:52:24 +0800

    Implement Image Upload

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -5,6 +5,7 @@ import { BiPoll } from 'react-icons/bi'
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5'
 import TabItem from './TabItem'
 import TextInputs from './PostForm/TextInputs'
+import ImageUpload from './PostForm/ImageUpload'
 
 const formTabs: TabItem[] = [
     {
@@ -45,7 +46,19 @@ const NewPostForm = () => {
 
     const handleCreatePost = async () => { };
 
-    const onSelectImage = () => { };
+    const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const reader = new FileReader();
+
+        if (event.target.files?.[0]) {
+            reader.readAsDataURL(event.target.files[0]);
+        }
+
+        reader.onload = (readerEvent) => {
+            if (readerEvent.target?.result) {
+                setSelectedFile(readerEvent.target.result as string);
+            }
+        };
+    };
 
     const onTextChange = (event: React.ChangeEvent<HTMLInputElement> | HTMLTextAreaElement) => {
         const { target: { name, value } } = event;
@@ -73,6 +86,14 @@ const NewPostForm = () => {
                         onChange={onTextChange}
                         loading={loading} />
                 )}
+                {
+                    selectedTab === "Images & Video" && (
+                        <ImageUpload selectedFile={selectedFile}
+                            onSelectImage={onSelectImage}
+                            setSelectedTab={setSelectedTab}
+                            setSelectedFile={setSelectedFile} />
+                    )
+                }
             </Flex>
         </Flex>
     )
