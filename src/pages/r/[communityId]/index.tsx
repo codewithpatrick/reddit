51de289214890e07fb93b7c0751a0ae2395f8eac
Patrick Pangilinan <programmingwithpatrick@gmail.com>
2024-04-06 20:24:05 +0800

    Implement Text Inputs for Post

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,4 +1,5 @@
 import { Community } from '@/src/atoms/communitiesAtom';
+import CreatePostLink from '@/src/components/Community/CreatePostLink';
 import Header from '@/src/components/Community/Header';
 import CommunityNotFound from '@/src/components/Community/NotFound';
 import PageContent from '@/src/components/Layout/PageContent';
@@ -22,9 +23,7 @@ const CommunityPage = ({ communityData }: CommunityPageProps) => {
             <Header communityData={communityData} />
             <PageContent>
                 <>
-                    <div>
-                        LEFT
-                    </div>
+                    <CreatePostLink />
                 </>
                 <>
                     <div>
