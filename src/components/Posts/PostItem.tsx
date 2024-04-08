442995f4ea8d5352263db4030fa9c7d13907f186
Patrick Pangilinan <programmingwithpatrick@gmail.com>
2024-04-08 16:26:40 +0800

    Implement Deleting Posts

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -11,7 +11,18 @@ import {
   IoArrowUpCircleSharp,
   IoBookmarkOutline,
 } from "react-icons/io5";
-import { Flex, Icon, Stack, Text, Image, Skeleton } from "@chakra-ui/react";
+import {
+  Flex,
+  Icon,
+  Stack,
+  Text,
+  Image,
+  Skeleton,
+  Spinner,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+} from "@chakra-ui/react";
 import moment from "moment";
 
 type PostItemProps = {
@@ -19,7 +30,7 @@ type PostItemProps = {
   userIsCreator: boolean;
   userVoteValue?: number;
   onVote: () => {};
-  onDeletePost: () => {};
+  onDeletePost: (post: Post) => Promise<boolean>;
   onSelectPost: () => void;
 };
 
@@ -32,6 +43,23 @@ const PostItem = ({
   onSelectPost,
 }: PostItemProps) => {
   const [loadingImage, setLoadingImage] = useState(true);
+  const [error, setError] = useState(false);
+  const [loadingDelete, setLoadingDelete] = useState(false);
+
+  const handleDelete = async () => {
+    setLoadingDelete(true);
+
+    try {
+      const success = await onDeletePost(post);
+
+      if (!success) throw new Error("Failed to delete post.");
+    } catch (error: any) {
+      console.log("handleDelete error", error);
+      setError(error.message);
+    }
+
+    setLoadingDelete(false);
+  };
 
   return (
     <Flex
@@ -74,6 +102,12 @@ const PostItem = ({
         />
       </Flex>
       <Flex direction="column" width="100%">
+        {error && (
+          <Alert status="error">
+            <AlertIcon />
+            <AlertTitle mr={2}>{error}</AlertTitle>
+          </Alert>
+        )}
         <Stack spacing={1} p="10px">
           <Stack direction="row" spacing={0.6} align="center" fontSize="9pt">
             <Text>
@@ -138,10 +172,16 @@ const PostItem = ({
               borderRadius={4}
               _hover={{ bg: "gray.200" }}
               cursor="pointer"
-              onClick={onDeletePost}
+              onClick={handleDelete}
             >
-              <Icon as={AiOutlineDelete} mr={2} />
-              <Text fontSize="9pt">Delete</Text>
+              {loadingDelete ? (
+                <Spinner size="sm" />
+              ) : (
+                <>
+                  <Icon as={AiOutlineDelete} mr={2} />
+                  <Text fontSize="9pt">Delete</Text>
+                </>
+              )}
             </Flex>
           )}
         </Flex>
--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { postState } from "../atoms/postsAtom";
+import { Post, postState } from "../atoms/postsAtom";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../firebase/clientApp";
+import { deleteDoc, doc } from "firebase/firestore";
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
@@ -9,7 +12,29 @@ const usePosts = () => {
 
   const onSelectPost = () => {};
 
-  const onDeletePost = async () => {};
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      // check if image, delete if exists
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        await deleteObject(imageRef);
+      }
+
+      //delete post document from firestore
+      const postDocRef = doc(firestore, "posts", post.id);
+      await deleteDoc(postDocRef);
+
+      // update recoil state
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((item) => item.id !== post.id),
+      }));
+    } catch (error: any) {
+      console.log("onDeletePost error", error);
+    }
+
+    return true;
+  };
 
   return {
     postStateValue,
