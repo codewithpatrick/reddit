73d33038fbac2a5f522c5c496acab1acad432a3a
Patrick Pangilinan <programmingwithpatrick@gmail.com>
2024-04-22 21:51:59 +0800

    Implement Directory Image Updates

--- src/components/Navbar/Directory/useDirectory.tsx
+++ src/components/Navbar/Directory/useDirectory.tsx
@@ -1,15 +1,18 @@
+import { communityState } from "@/src/atoms/communitiesAtom";
 import {
   DirectoryMenuItem,
   directoryMenuState,
 } from "@/src/atoms/directoryMenuAtom";
 import { useRouter } from "next/router";
-import React from "react";
-import { useRecoilState } from "recoil";
+import React, { useEffect } from "react";
+import { FaReddit } from "react-icons/fa";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 const useDirectory = () => {
   const [directoryState, setDirectoryState] =
     useRecoilState(directoryMenuState);
   const router = useRouter();
+  const communityStateValue = useRecoilValue(communityState);
 
   const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
     setDirectoryState((prev) => ({
@@ -30,6 +33,23 @@ const useDirectory = () => {
       isOpen: !directoryState.isOpen,
     }));
   };
+
+  useEffect(() => {
+    const { currentCommunity } = communityStateValue;
+
+    if (currentCommunity) {
+      setDirectoryState((prev) => ({
+        ...prev,
+        selectedMenuItem: {
+          displayText: `r/${currentCommunity.id}`,
+          link: `/r/${currentCommunity.id}`,
+          imageURL: currentCommunity.imageURL,
+          icon: FaReddit,
+          iconColor: "blue.500",
+        },
+      }));
+    }
+  }, [communityStateValue.currentCommunity]);
 
   return { directoryState, toggleMenuOpen, onSelectMenuItem };
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import SearchInput from './SearchInput'
-import { Flex, Image } from '@chakra-ui/react'
-import RightContent from './RightContent/RightContent'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '@/src/firebase/clientApp'
-import Directory from './Directory/Directory'
+import React from "react";
+import SearchInput from "./SearchInput";
+import { Flex, Image } from "@chakra-ui/react";
+import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
+import Directory from "./Directory/Directory";
+import useDirectory from "./Directory/useDirectory";
+import { defaultMenuItem } from "@/src/atoms/directoryMenuAtom";
 
 const Navbar = () => {
-    const [user, loading, error] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
 
-    return (
-        <>
-            <Flex
-                bg="white"
-                height="44px"
-                padding="6px 12px"
-                justify={{ md: "space-between" }}>
-                <Flex
-                    align="center"
-                    width={{ base: "40px", md: "auto" }}
-                    mr={{ base: 0, md: 2 }}>
-                    <Image src="/images/redditFace.svg"
-                        height="30px"
-                        alt="" />
-                    <Image src="/images/redditText.svg"
-                        height="46px"
-                        display={{ base: "none", md: "unset" }}
-                        alt="" />
-                </Flex>
-                {user && <Directory />}
-                <SearchInput user={user} />
-                <RightContent user={user} />
-            </Flex>
-        </>
-    )
-}
+  return (
+    <>
+      <Flex
+        bg="white"
+        height="44px"
+        padding="6px 12px"
+        justify={{ md: "space-between" }}
+      >
+        <Flex
+          align="center"
+          width={{ base: "40px", md: "auto" }}
+          mr={{ base: 0, md: 2 }}
+          cursor="pointer"
+          onClick={() => onSelectMenuItem(defaultMenuItem)}
+        >
+          <Image src="/images/redditFace.svg" height="30px" alt="" />
+          <Image
+            src="/images/redditText.svg"
+            height="46px"
+            display={{ base: "none", md: "unset" }}
+            alt=""
+          />
+        </Flex>
+        {user && <Directory />}
+        <SearchInput user={user} />
+        <RightContent user={user} />
+      </Flex>
+    </>
+  );
+};
 
 export default Navbar;
